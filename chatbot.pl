d8ff3584023c95a622b3260aa18395434ea4eb0e
kiefferdy <kieffer_recatody@dlsu.edu.ph>
2023-04-10 21:52:31 +0800

    Updated diseases list and fixed bugs

--- chatbot.pl
+++ chatbot.pl
@@ -65,24 +65,99 @@ DEFINITIONS
 
 % Defines symptoms (preferably shuffled)
 symptom(cough).
-symptom(fever).
 symptom(chest_pain).
 symptom(runny_nose).
 symptom(sore_throat).
+symptom(weak).
+symptom(appetite_loss).
+symptom(fatigue).
+symptom(confusion).
+symptom(hard_swallow).
+symptom(irritable).
+symptom(nausea).
+symptom(headache).
+symptom(bodyache).
+symptom(abdominal_pain).
+symptom(abdominal_cramps).
+symptom(stomach_pain).
+symptom(muscle_pain).
+symptom(joint_pain).
+symptom(rash).
+symptom(colds).
+symptom(hard_breathe).
+symptom(chills).
+symptom(diarrhea).
+symptom(watery_diarrhea).
+symptom(vomiting).
+symptom(no_taste).
+symptom(thirst).
+symptom(leg_cramps).
+symptom(rapid_heart).
+symptom(loss_skin_elasticity).
+symptom(dry_mucous_membrane).
+symptom(low_bp).
+symptom(bloating).
+symptom(bloody_poo).
+symptom(pus_poo).
+symptom(pain_behind_eyes).
+symptom(swollen_glands).
+symptom(had_dengue).
+symptom(constipation).
+symptom(night_sweats).
+symptom(blood_cough).
+symptom(phlegm).
+symptom(weight_loss).
+symptom(blisters).
+symptom(insomnia).
+symptom(anxiety).
+symptom(partial_paralysis).
+symptom(hyperactivity).
+symptom(hallucinations).
+symptom(more_saliva).
+symptom(weight_loss).
+symptom(pink_eye).
+symptom(feel_unwell).
 
 % Defines diseases and their symptoms
-disease(pneumonia, [cough, fever, chest_pain]).
-disease(covid, [runny_nose, sore_throat]).
+disease(tuberculosis, [cough, fever, weak, night_sweats, chest_pain, weight_loss, appetite_loss, blood_cough, phlegm]).
+disease(typhoid_fever, [fever, weak, stomach_pain, headache, diarrhea, constipation, cough, appetite_loss, rash]).
+disease(dengue, [fever, headache, pain_behind_eyes, muscle_pain, joint_pain, nausea, vomiting, swollen_glands, rash]).
+disease(flu, [fever, chills, cough, sore_throat, colds, muscle_pain, bodyache, headache, fatigue, vomiting, diarrhea]).
+disease(stomach_flu, [fever, appetite_loss, bloating, nausea, vomiting, stomach_pain, abdominal_pain, abdominal_cramps, diarrhea, blood_poo, pus_poo, feel_unwell]).
+disease(cholera, [diarrhea, watery_diarrhea, vomiting, thirst, leg_cramps, irritable, loss_skin_elasticity, dry_mucous_membrane]).
+disease(rabies, [insomnia, anxiety, confusion, partial_paralysis, hyperactivity, irritable, hallucinations, more_saliva, hard_swallow]).
+disease(pneumonia, [fever, cough, chest_pain, phlegm, blood_cough, chills, hard_breathe, appetite_loss, fatigue, nausea, vomiting, confusion]).
+disease(covid, [fever, colds, sore_throat, cough, no_taste, no_smell, hard_breathe, chills, headache, chest_pain, pink_eye, nausea, vomiting, diarrhea, rash]).
+disease(hand_foot_mouth, [fever, sore_throat, feel_unwell, rash, irritable, appetite_loss]).
 
 % Defines diagnostic tests or other information associated with the illness
 question(old, 'Are you old?').
+question(young, 'Are you a young adult/adolescent (less than 24 yrs old)?').
+question(child, 'Are you a child?').
 question(smoker, 'Do you smoke?').
+question(had_dengue, 'Have you had dengue before?').
+
 question(low_oxygen, 'Measure your blood oxygen. Is it low?').
+question(low_bp, 'Measure your blood pressure. Is it low?').
+question(rapid_heart, 'Measure your heart rate. Is it high?').
+
 question(antigen_positive, 'Perform a rapid antigen test. Is the result positive?').
+question(animal_wound, 'Have you been bitten or scratched by an animal?').
+question(exposure, 'Have you been exposed to sick people?').
+question(blister, 'Examine mouth. Are there any painful, red, blister-like lesions?').
+question(pink_eye, 'Examine eye. Is there swelling and/or redness?').
 
 % Defines which information can be used to confirm which disease
-confirm(pneumonia, [old, smoker, low_oxygen]).
-confirm(covid, [antigen_positive]).
+confirm(tuberculosis, [child, young, exposure]).
+confirm(typhoid_fever, []).
+confirm(dengue, [had_dengue]).
+confirm(flu, [child, old]).
+confirm(stomach_flu, []).
+confirm(cholera, [low_bp, rapid_heart]).
+confirm(rabies, [animal_wound]).
+confirm(pneumonia, [old, smoker, low_oxygen, exposure]).
+confirm(covid, [antigen_positive, exposure]).
+confirm(hand_foot_mouth, [blister, child]).
 
 /*
 RULES
@@ -144,32 +219,6 @@ diagnose :-
         verify(Y)
     )
     ;
-    % Backtracks from verify/1 and determines the accuracy of the diagnosis
-    (
-        user_info(Information),
-        confirm(Y, DiseaseInfo),
-        count_matching_items(Information, DiseaseInfo, H),
-        length(DiseaseInfo, I),
-        H >= 1, H < I,
-        capitalize_first_letter(Y, YC),
-        write(YC), write(' is likely your disease. Please consult a doctor.')
-        ;
-        user_info(Information),
-        confirm(Y, DiseaseInfo),
-        count_matching_items(Information, DiseaseInfo, H),
-        length(DiseaseInfo, I),
-        H =:= I,
-        capitalize_first_letter(Y, YC),
-        write(YC), write(' is indeed most likely your disease. Please consult a doctor.')
-        ;
-        user_info(Information),
-        confirm(Y, DiseaseInfo),
-        count_matching_items(Information, DiseaseInfo, H),
-        H =:= 0,
-        capitalize_first_letter(Y, YC),
-        write(YC), write(' may not actually be your disease. Please consult a doctor.')
-    )
-    ;
     % If no diagnosis is found
     write('Sorry, we could not determine a diagnosis based on your symptoms. Please consult a doctor.'), nl.
 
@@ -182,7 +231,38 @@ verify(Disease) :-
     (Response == 'yes' -> add_to_user_info(Information); add_to_rejected_info(Information)),
     verify(Disease)
     ;
-    fail.
+    conclude(Disease).
+
+% Determines the accuracy of the diagnosis
+conclude(Disease) :-
+    user_info(Information),
+    confirm(Disease, DiseaseInfo),
+    count_matching_items(Information, DiseaseInfo, H),
+    length(DiseaseInfo, I),
+    ((H >= 1, H < I); (H =:= 1, I =:= 1)), 
+    capitalize_first_letter(Disease, DiseaseC),
+    write(DiseaseC), write(' is likely your disease. Please consult a doctor.')
+    ;
+    user_info(Information),
+    confirm(Disease, DiseaseInfo),
+    count_matching_items(Information, DiseaseInfo, H),
+    length(DiseaseInfo, I),
+    H > 1, H =:= I,
+    capitalize_first_letter(Disease, DiseaseC),
+    write(DiseaseC), write(' is indeed most likely your disease. Please consult a doctor.')
+    ;
+    user_info(Information),
+    confirm(Disease, DiseaseInfo),
+    count_matching_items(Information, DiseaseInfo, H),
+    length(DiseaseInfo, I),
+    H =:= 0, I >= 1,
+    capitalize_first_letter(Disease, DiseaseC),
+    write(DiseaseC), write(' may not actually be your disease. Please consult a doctor.')
+    ;
+    confirm(Disease, DiseaseInfo),
+    length(DiseaseInfo, I),
+    I =:= 0,
+    write('Verifying whether or not you have '), write(Disease), write(' requires laboratory tests. Please consult a doctor and refer to a large medical facility.').
 
 % Runs the program upon consultation by calling diagnose/0
 :- diagnose.
